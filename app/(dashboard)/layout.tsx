919ec2d7fa99cc217e63dd82f8d5632e666529bf
akins-dev <bobemmanuel503@gmail.com>
2023-09-05 04:43:28 +0100

    chore: started sidebar component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from '@/components/Navbar'
+import Sidebar from '@/components/Sidebar'
 
 const DashboardLayout = ({
     children
@@ -6,9 +7,7 @@ const DashboardLayout = ({
   return (
     <div className="h-full relative">
         <div className='hidden h-full md:flex md:flex-col md:fixed md:inset-y-0 z-[80] md:w-72 bg-gray-900'>
-            <div>
-                Hello Sidebar
-            </div>
+            <Sidebar />
         </div>
         <main className="md:pl-72">
             <Navbar />
