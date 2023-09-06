dee98d05ff68a96a05b6bb2554d38d3327fcf185
akins-dev <bobemmanuel503@gmail.com>
2023-09-06 04:38:42 +0100

    chore: created dashboardlink on sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,12 +1,22 @@
 "use client"
 
-import { Montserrat } from "next/font/google"
+import { Montserrat, Roboto_Mono } from "next/font/google"
 import Image from "next/image"
 import Link from "next/link"
 
 import { cn } from "@/lib/utils"
+import { LayoutDashboard } from "lucide-react"
 
-const montserrat = Montserrat({ weight: "600", subsets: ["latin"]})
+const montserrat = Roboto_Mono({ weight: "600", subsets: ["latin"]})
+
+const routes = [
+    {
+        label: "Dashboard",
+        icon: LayoutDashboard,
+        href: "/dashboard",
+        color: "text-sky-500"
+    }
+]
 
 
 const Sidebar = () => {
@@ -25,6 +35,23 @@ const Sidebar = () => {
                     Alpha
                 </h1>
             </Link>
+            <div className="sapce-y-1">
+                {routes.map((route) => (
+                    <Link
+                        href={route.href}
+                        key={route.href}
+                        className="
+                        text-sm group flex p-3 w-full justify-start
+                        font-medium cursor-pointer hover:text-white 
+                        hover:bg-white/10 rounded-lg transition"
+                    >
+                        <div className="flex items-center flex-1">
+                            <route.icon className={cn("h-5 w-5 mr-3", route.color)}/>
+                            {route.label}
+                        </div>
+                    </Link>
+                ))}
+            </div>
         </div>
     </div>
   )
