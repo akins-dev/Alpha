ec89e86b58755ae872b0b9f527797b79964c1e47
akins-dev <bobemmanuel503@gmail.com>
2023-09-06 04:47:27 +0100

    chore: added other routes to side bar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 import Link from "next/link"
 
 import { cn } from "@/lib/utils"
-import { LayoutDashboard } from "lucide-react"
+import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react"
 
 const montserrat = Roboto_Mono({ weight: "600", subsets: ["latin"]})
 
@@ -15,6 +15,41 @@ const routes = [
         icon: LayoutDashboard,
         href: "/dashboard",
         color: "text-sky-500"
+    },
+    {
+        label: "Conversation",
+        icon: MessageSquare,
+        href: "/conversation",
+        color: "text-violet-500"
+    },
+    {
+        label: "Image Generation",
+        icon: ImageIcon,
+        href: "/image",
+        color: "text-pink-700"
+    },
+    {
+        label: "Video Generation",
+        icon: VideoIcon,
+        href: "/video",
+        color: "text-orange-700"
+    },
+    {
+        label: "Music Generation",
+        icon: Music,
+        href: "/music",
+        color: "text-emerald-500"
+    },
+    {
+        label: "Code Generation",
+        icon: Code,
+        href: "/code",
+        color: "text-green-500"
+    },
+    {
+        label: "Settings",
+        icon: Settings,
+        href: "/settings",
     }
 ]
 
@@ -36,7 +71,7 @@ const Sidebar = () => {
                 </h1>
             </Link>
             <div className="sapce-y-1">
-                {routes.map((route) => (
+                {routes?.map((route) => (
                     <Link
                         href={route.href}
                         key={route.href}
